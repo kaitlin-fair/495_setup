a6f330d3d5925595d3a269dae4daeb7fba98617d
KFair <kaitlin.fair@afacademy.af.edu>
2024-01-04 14:17:47 -0700

    changed to single neuron

---
+++ single_neuron.py
@@ -0,0 +1,69 @@
+## Code cred: 
+## https://www.nengo.ai/nengo/examples/basic/single-neuron.html
+
+## Setup tools
+import matplotlib.pyplot as plt
+import numpy as np
+
+import nengo
+from nengo.utils.matplotlib import rasterplot
+
+from nengo.dists import Uniform
+
+## Create Neuron
+model = nengo.Network(label="A Single Neuron")
+with model:
+    neuron = nengo.Ensemble(
+        1,
+        dimensions=1,  # Represent a scalar
+        # Set intercept to 0.5
+        intercepts=Uniform(-0.5, -0.5),
+        # Set the maximum firing rate of the neuron to 100hz
+        max_rates=Uniform(100, 100),
+        # Set the neuron's firing rate to increase for positive input
+        encoders=[[1]],
+    )
+
+## Create Input
+with model:
+    cos = nengo.Node(lambda t: np.cos(8 * t))
+
+## Connect Input to Neuron
+with model:
+    # Connect the input signal to the neuron
+    nengo.Connection(cos, neuron)
+
+## Add probes to view neuron response
+with model:
+    # The original input
+    cos_probe = nengo.Probe(cos)
+    # The raw spikes from the neuron
+    spikes = nengo.Probe(neuron.neurons)
+    # Subthreshold soma voltage of the neuron
+    voltage = nengo.Probe(neuron.neurons, "voltage")
+    # Spikes filtered by a 10ms post-synaptic filter
+    filtered = nengo.Probe(neuron, synapse=0.01)
+
+## Run model
+with nengo.Simulator(model) as sim:  # Create the simulator
+    sim.run(1)  # Run it for 1 second
+
+## Plot the results!
+# Plot the decoded output of the ensemble
+plt.figure()
+plt.plot(sim.trange(), sim.data[filtered])
+plt.plot(sim.trange(), sim.data[cos_probe])
+plt.xlim(0, 1)
+
+# Plot the spiking output of the ensemble
+plt.figure(figsize=(10, 8))
+plt.subplot(221)
+rasterplot(sim.trange(), sim.data[spikes])
+plt.ylabel("Neuron")
+plt.xlim(0, 1)
+
+# Plot the soma voltages of the neurons
+plt.subplot(222)
+plt.plot(sim.trange(), sim.data[voltage][:, 0], "r")
+plt.xlim(0, 1)
+plt.show()
